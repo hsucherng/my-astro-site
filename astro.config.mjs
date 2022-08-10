5ee3a6237adea8d365db960ef80a1b841e3c860d
Hsu-Cherng <hsucherng.chong@gmail.com>
2022-08-10 16:08:37 +0800

    Init repo

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+import netlify from '@astrojs/netlify/functions';
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [svelte()],
+  adapter: netlify()
+});
---
+++ src/components/Tester.svelte
@@ -0,0 +1,16 @@
+<script>
+  let name = 'world';
+  let count = 0;
+
+  function incrementCount() {
+    count += 1;
+  }
+</script>
+
+<h1>Hello {name}!</h1>
+
+<button on:click={incrementCount}>Clicked {count} times</button>
+
+<style>
+  h1 { font-size: 1em; }
+</style>
